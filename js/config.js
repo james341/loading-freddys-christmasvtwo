06b53bc160c7d8654070b618611bfc23d6d6535f
james341 <taine049@gmail.com>
2021-12-05 12:48:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -108,17 +108,11 @@ var l_bgImages = [
 	"image34.jpg",
 	"image35.jpg",
 	"image36.jpg",
-    "image37.jpg",
+        "image37.jpg",
 	"image38.jpg",
 	"image39.jpg",
 	"image40.jpg",
 	"image41.jpg",
-	"image42.jpg",
-	"image43.jpg",
-	"image44.jpg",
-	"image45.jpg",
-	"image46.jpg",
-	"image47.jpg",
 ];
 
 // (Image-background only)
