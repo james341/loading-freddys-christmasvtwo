952bde741dfeb6b463ac2664fcc955f64a9ba960
james341 <taine049@gmail.com>
2021-12-27 15:29:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -140,7 +140,6 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "workatapizzaplacechristmas.ogg", name: "Nations - Installation Music"},
 	{ogg: "whamlastchristmas.ogg", name: "Nations - Installation Music"},
 	{ogg: "davethemeend3.ogg", name: "Nations - Installation Music"},
 ];
