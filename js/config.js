aa098a372103dff0a41d70e27c306dd57600b362
james341 <taine049@gmail.com>
2025-12-14 04:28:55 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -153,11 +153,13 @@ var l_musicDisplay = true;
 var l_musicPlaylist = [
 	{ogg: "arianagrandesantatellmeinstrumental.ogg", name: "{AN} - Installation Music"},
 	{ogg: "thetrapezistjoMogK0wzXI.ogg", name: "{AN} - Installation Music"},
+	{ogg: "whamlastchristmasInstrumental.ogg", name: "{AN} - Installation Music"},
 	{ogg: "chdsaflipsidetheme.ogg", name: "{AN} - Installation Music"},
 	{ogg: "christmasIgaveyoumyheart.ogg", name: "{AN} - Installation Music"},
 	{ogg: "christmasmusicholidaysnocr.ogg", name: "{AN} - Installation Music"},
 	{ogg: "christmasvillage.ogg", name: "{AN} - Installation Music"},
 	{ogg: "royaltyfreebackgroundmusicvideos.ogg", name: "{AN} - Installation Music"},
+	{ogg: "iwantchristmasInstrumental.ogg", name: "{AN} - Installation Music"},
 	{ogg: "workatapizzaplacechristmas.ogg", name: "{AN} - Installation Music"},
 	{ogg: "noclopyrightchristmasbackgroundmusic.ogg", name: "{AN} - Installation Music"},
 	{ogg: "christmas.ogg", name: "{AN} - Installation Music"},
@@ -308,6 +310,7 @@ var l_showErrorsIngame = true;
 var checkConfigFile = function() {
 	return true;
 };
+
 
 
 
